49445d7ac13eaeb55fee90ac1673a664ae11fc22
davidinmichael <davidinmichael@gmail.com>
2024-11-19 16:30:13 +0100

    Added Home and About pages.

--- core/views.py
+++ core/views.py
@@ -5,3 +5,8 @@ from django.views import View
 class HomePage(View):
 	def get(self, request):
 		return render(request, "core/index.html")
+
+
+class AboutPage(View):
+	def get(self, request):
+		return render(request, "core/about.html")
