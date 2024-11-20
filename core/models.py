f895803a32b075e69402014657a6a5ec139b30f6
davidinmichael <davidinmichael@gmail.com>
2024-11-20 17:06:37 +0100

    Added the product model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Category(models.TextChoices):
+	dell = "dell", _("Dell")
+	hp = "hp", _("HP")
+	accessory = "accessory", _("Accessory")
+	software = "software", _("Software")
+	other = "other", _("Other")
+
+
+class Availability(models.TextChoices):
+	available = "available", _("Available")
+	unavailable = "unavailable", _("Unavailable")
+
+class BaseModel(models.Model):
+	created_at = models.DateField(default=timezone.now)
+	updated_at = models.DateField(auto_now=True)
+
+	class Meta:
+		abstract = True
+
+class Product(BaseModel):
+	name = models.CharField(max_length=50)
+	description = models.TextField()
+	price = models.DecimalField()
+	front_img = models.ImageField(upload_to="products/", null=True, blank=True)
+	back_img = models.ImageField(upload_to="products/", null=True, blank=True)
+	status = models.CharField(max_length=50, null=True, blank=True, choices=Availability.choices, default=Availability.available)
+	category = models.CharField(max_length=50, null=True, blank=True, choices=Category.choices, default=Category.other)
+
